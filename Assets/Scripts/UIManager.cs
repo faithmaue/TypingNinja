aa4b6e12fda1b5048855c4632a640870528336cf
Melaina Stapley <melaina.s3103@gmail.com>
2025-12-02 15:26:28 -0600

    removed skins button, adjusted sizing of main menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
@@ -14,54 +12,22 @@ public class UIManager : MonoBehaviour
 
     public GameObject singlePlayerButton;
     public GameObject multiPlayerButton;
-    public GameObject skinsButton;
     public GameObject leaderboardButton;
+
     public string player1Name = "none";
     public string player2Name = "none";
 
-    
-    private string[] skins = { "Blue", "Black", "Red" };
-    private int index = 0;
     private int numPlayers = 0;
     private int playersReady = 0;
 
-    // No static here; and don't initialize from DataManager at field level
-    private SaveData save;
-    private string skin;
-    private List<PlayerRecord> history;
-
     void Start()
     {
-        // Get the save data AFTER Unity is running
-        save = DataManager.Data;
-        skin = save.selectedSkin;
-        history = save.history;
-
+        // Hide name + ready UI until a mode is chosen
         player1NameField.gameObject.SetActive(false);
         player2NameField.gameObject.SetActive(false);
 
         player1ReadyButton.SetActive(false);
         player2ReadyButton.SetActive(false);
-
-        for (int i = 0; i < skins.Length; i++)
-        {
-            if (skins[i] == save.selectedSkin)
-                index = i;
-        }
-    }
-
-    public void NextSkin()
-    {
-        index = (index + 1) % skins.Length;
-        save.selectedSkin = skins[index];
-        DataManager.Save();
-    }
-
-    public void PrevSkin()
-    {
-        index = (index - 1 + skins.Length) % skins.Length;
-        save.selectedSkin = skins[index];
-        DataManager.Save();
     }
 
     public void SinglePlayerName()
@@ -89,15 +55,12 @@ public class UIManager : MonoBehaviour
     public void ReadyForGame()
     {
         playersReady++;
+
         if (player1NameField != null)
-        {
             player1Name = player1NameField.text;
-        }
+
         if (player2NameField != null)
-        {
             player2Name = player2NameField.text;
-        }
-       // Debug.Log("Player 1 name: " + player1Name);
 
         if (playersReady == numPlayers)
         {
@@ -120,7 +83,6 @@ public class UIManager : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
     }
-
 
     public void Quit()
     {
